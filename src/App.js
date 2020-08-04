86fde50232a1bbe206591cecb12549f073fe587e
JUNGRAN <jr09h51@gmail.com>
2020-08-04 17:35:23 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+//import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+    
+ /*   constructor(props){
+        super(props);
+        console.log("hello I'm a constructor"); //render 보다 먼저 호출 됨
+                              //component가 mount 될 때(=스크린에 표시될 때) constructor를 호출한다.
+    }
+    state={
+        // "state" contains dynamic values of data. 변하는 데이터를 가지고 있음.
+        count : 0
+    };
+
+   add = () => {
+    //매번 state의 상태를 변경할 때 render function 을 호출해서 refresh 해야 함. 
+    // 따라서 setState을 써야 함. setState이 호출되면 새로운 state과 함께 자동으로 render function 호출함.
+    this.setState(current => ({count: current.count + 1}));
+   };
+   
+   minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+   };
+
+   componentDidMount(){
+       console.log('component rendered');
+   }
+
+   componentDidUpdate(){
+    console.log('component updated'); //render 다음에 호출됨.
+   }
+
+   componentWillUnmount(){
+       console.log('Goodbye cruel world');
+   } */
+    state = {
+        isLoading : true
+        , movies :[]
+    };
+
+    componentDidMount(){
+        // component가 mount 되자마자 호출 됨
+        setTimeout(()=>{
+            this.setState({isLoading:false});
+        }, 6000);
+    }
+
+    render(){
+        //react는 "자동으로" class component의 render method를 실행한다. 
+        
+        const {isLoading} = this.state;
+        
+        return <div>
+                   {isLoading ? "Loading" : "We are ready" }
+                </div>
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// <App /> 따위의 것들 : component. react는 component와 함께 동작함. component를 사용하고자 할 때.
+// 꼭 <App > 형태여야 함. React는 components를 사용해서 HTML처럼 작성하려는 경우에 필요.
+// component는 HTML을 반환하는 함수. data를 보여주는 요소들임.
+
+
+/*
+ - JSX = Javascript + HTML. React에 특화된 개념.
+ - REACT Application은 한 번에 하나의 component만 rendering 할 수 있음. 따라서 모든 것이 App 안에 들어가야 함.
+
+ 
+ */
