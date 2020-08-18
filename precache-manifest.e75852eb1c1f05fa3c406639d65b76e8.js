c4e8ff04a320361e3547f83ca13b44b74d4d47e1
JUNGRAN <jr09h51@gmail.com>
2020-08-18 16:27:18 +0900

    Updates

---
+++ precache-manifest.e75852eb1c1f05fa3c406639d65b76e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eeee1a6d38511a276c03f36430cdb70",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "404dfe7787ce4f71b43f",
+    "url": "/movie_app/static/css/main.a766dcaa.chunk.css"
+  },
+  {
+    "revision": "a4987c1aed0faee1d8f9",
+    "url": "/movie_app/static/js/2.4c35eb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.4c35eb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404dfe7787ce4f71b43f",
+    "url": "/movie_app/static/js/main.30eecc93.chunk.js"
+  },
+  {
+    "revision": "b9cb9a6795ceb34c84cd",
+    "url": "/movie_app/static/js/runtime-main.f01f2091.js"
+  }
+]);
